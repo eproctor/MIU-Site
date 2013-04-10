ea2dda95ac6702f7e9a4d18fbbfa51ca057e08e7
E Proctor <eproctor@fullsail.edu>
2013-04-09 20:14:49 -0400

    Working Files with JSON Data for Week 1

---
+++ Week1a/js/json.js
@@ -0,0 +1,323 @@
+/* Name: Eugene Proctor :: Project 1 :: VFW 1303 */
+ 
+/* ========== JSON Object - Dummy Data for Testing ========== */
+
+
+var json = {
+
+		"poc1": {
+			"startdate": 		["Todays Date: ", "2013-01-27"],
+			"fullName":	 		["First Name: ", "Paul"],
+			"email":			["Email: ","paul@gmail.com"],
+			"city":				["Travel City: ","florida"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","guardian"],			
+			"description":		["Leave a Comment: ","This is a test for Paul"],
+			"rating":			["Rate your Comfort Level: ","99"]
+			
+			
+			
+
+				
+		},
+
+		"poc2": {
+			"startdate": 		["Todays Date: ", "2012-03-31"],
+			"fullName":	 		["First Name: ", "Willie"],
+			"email":			["Email: ","Willie@gmail.com"],
+			"city":				["Travel City: ","charlotte"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","parent"],			
+			"description":		["Leave a Comment: ","This is a test for Willie"],
+			"rating":			["Rate your Comfort Level: ","100"]
+						
+
+
+
+				
+		},
+		
+		
+		"poc3": {
+			"startdate": 		["Todays Date: ", "2013-01-02"],
+			"fullName":	 		["First Name: ", "Anthony"],
+			"email":			["Email: ","anthony@gmail.com"],
+			"city":				["Travel City: ","florida"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","guardian"],			
+			"description":		["Leave a Comment: ","This is a test for Anthony"],
+			"rating":			["Rate your Comfort Level: ","79"]
+			
+			
+			
+
+				
+		},
+
+		"poc4": {
+			"startdate": 		["Todays Date: ", "2012-05-05"],
+			"fullName":	 		["First Name: ", "Tom"],
+			"email":			["Email: ","tom@gmail.com"],
+			"city":				["Travel City: ","charlotte"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","brother"],			
+			"description":		["Leave a Comment: ","This is a test for Tom"],
+			"rating":			["Rate your Comfort Level: ","37"]
+						
+
+
+
+				
+		},
+		
+		
+		
+		"poc5": {
+			"startdate": 		["Todays Date: ", "2012-01-27"],
+			"fullName":	 		["First Name: ", "Gregg"],
+			"email":			["Email: ","gregg@gmail.com"],
+			"city":				["Travel City: ","florida"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","guardian"],			
+			"description":		["Leave a Comment: ","This is a test for Gregg"],
+			"rating":			["Rate your Comfort Level: ","58"]
+			
+			
+			
+
+				
+		},
+
+		"poc6": {
+			"startdate": 		["Todays Date: ", "2011-06-27"],
+			"fullName":	 		["First Name: ", "Jennifer"],
+			"email":			["Email: ","jennifer@gmail.com"],
+			"city":				["Travel City: ","charlotte"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","aunt"],			
+			"description":		["Leave a Comment: ","This is a test for Jennifer"],
+			"rating":			["Rate your Comfort Level: ","91"]
+						
+
+
+
+				
+		},
+		
+		
+		
+		"poc7": {
+			"startdate": 		["Todays Date: ", "2012-02-27"],
+			"fullName":	 		["First Name: ", "Katie"],
+			"email":			["Email: ","katie@gmail.com"],
+			"city":				["Travel City: ","florida"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","sister"],			
+			"description":		["Leave a Comment: ","This is a test for Katie"],
+			"rating":			["Rate your Comfort Level: ","23"]
+			
+			
+			
+
+				
+		},
+
+		"poc8": {
+			"startdate": 		["Todays Date: ", "2013-08-15"],
+			"fullName":	 		["First Name: ", "Michael"],
+			"email":			["Email: ","michael@gmail.com"],
+			"city":				["Travel City: ","charlotte"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","brother"],			
+			"description":		["Leave a Comment: ","This is a test for Michael"],
+			"rating":			["Rate your Comfort Level: ","54"]
+						
+
+
+
+				
+		},
+		
+		
+		"poc9": {
+			"startdate": 		["Todays Date: ", "2013-01-27"],
+			"fullName":	 		["First Name: ", "Sam"],
+			"email":			["Email: ","sam@gmail.com"],
+			"city":				["Travel City: ","atlanta"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","guardian"],			
+			"description":		["Leave a Comment: ","This is a test for Sam"],
+			"rating":			["Rate your Comfort Level: ","42"]
+			
+			
+			
+
+				
+		},
+
+		"poc10": {
+			"startdate": 		["Todays Date: ", "2012-03-12"],
+			"fullName":	 		["First Name: ", "Jody"],
+			"email":			["Email: ","jody@gmail.com"],
+			"city":				["Travel City: ","charlotte"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","brother"],			
+			"description":		["Leave a Comment: ","This is a test for Jody"],
+			"rating":			["Rate your Comfort Level: ","33"]
+						
+
+
+
+				
+		},
+		
+		
+		
+		"poc11": {
+			"startdate": 		["Todays Date: ", "2013-01-02"],
+			"fullName":	 		["First Name: ", "Heather"],
+			"email":			["Email: ","heather@gmail.com"],
+			"city":				["Travel City: ","florida"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","guardian"],			
+			"description":		["Leave a Comment: ","This is a test for Heather"],
+			"rating":			["Rate your Comfort Level: ","77"]
+			
+			
+			
+
+				
+		},
+
+		"poc12": {
+			"startdate": 		["Todays Date: ", "2013-03-27"],
+			"fullName":	 		["First Name: ", "Dexter"],
+			"email":			["Email: ","dexter@gmail.com"],
+			"city":				["Travel City: ","atlanta"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","brother"],			
+			"description":		["Leave a Comment: ","This is a test for Dexter"],
+			"rating":			["Rate your Comfort Level: ","81"]
+						
+
+
+
+				
+		},
+		
+		
+		"poc13": {
+			"startdate": 		["Todays Date: ", "2012-09-14"],
+			"fullName":	 		["First Name: ", "David"],
+			"email":			["Email: ","david@gmail.com"],
+			"city":				["Travel City: ","florida"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","guardian"],			
+			"description":		["Leave a Comment: ","This is a test for David"],
+			"rating":			["Rate your Comfort Level: ","19"]
+			
+			
+			
+
+				
+		},
+
+		"poc14": {
+			"startdate": 		["Todays Date: ", "2012-04-17"],
+			"fullName":	 		["First Name: ", "Kimberly"],
+			"email":			["Email: ","kimberly@gmail.com"],
+			"city":				["Travel City: ","charlotte"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","sister"],			
+			"description":		["Leave a Comment: ","This is a test for Kimberly"],
+			"rating":			["Rate your Comfort Level: ","65"]
+						
+
+
+
+				
+		},
+		
+		
+		"poc15": {
+			"startdate": 		["Todays Date: ", "2012-01-27"],
+			"fullName":	 		["First Name: ", "John"],
+			"email":			["Email: ","john@gmail.com"],
+			"city":				["Travel City: ","atlanta"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","guardian"],			
+			"description":		["Leave a Comment: ","This is a test for John"],
+			"rating":			["Rate your Comfort Level: ","39"]
+			
+			
+			
+
+				
+		},
+
+		"poc16": {
+			"startdate": 		["Todays Date: ", "2013-04-22"],
+			"fullName":	 		["First Name: ", "Bill"],
+			"email":			["Email: ","bille@gmail.com"],
+			"city":				["Travel City: ","charlotte"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","brother"],			
+			"description":		["Leave a Comment: ","This is a test for Bill"],
+			"rating":			["Rate your Comfort Level: ","60"]
+						
+
+
+
+				
+		},
+		
+		
+		"poc17": {
+			"startdate": 		["Todays Date: ", "2012-03-27"],
+			"fullName":	 		["First Name: ", "Marvin"],
+			"email":			["Email: ","marvin@gmail.com"],
+			"city":				["Travel City: ","florida"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","guardian"],			
+			"description":		["Leave a Comment: ","This is a test for Marvin"],
+			"rating":			["Rate your Comfort Level: ","29"]
+			
+			
+			
+
+				
+		},
+
+		"poc18": {
+			"startdate": 		["Todays Date: ", "2011-03-20"],
+			"fullName":	 		["First Name: ", "Susan"],
+			"email":			["Email: ","susan@gmail.com"],
+			"city":				["Travel City: ","atlanta"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","sister"],			
+			"description":		["Leave a Comment: ","This is a test for Susan"],
+			"rating":			["Rate your Comfort Level: ","100"]
+						
+
+
+
+				
+		},
+		
+		
+		"poc19": {
+			"startdate": 		["Todays Date: ", "2013-01-27"],
+			"fullName":	 		["First Name: ", "Charlie"],
+			"email":			["Email: ","charlie@gmail.com"],
+			"city":				["Travel City: ","florida"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","guardian"],			
+			"description":		["Leave a Comment: ","This is a test for Charlie"],
+			"rating":			["Rate your Comfort Level: ","10"]
+			
+			
+			
+
+				
+		},
+
+
+
+		"poc20": {
+			"startdate": 		["Todays Date: ", "2013-04-27"],
+			"fullName":	 		["First Name: ", "Sam"],
+			"email":			["Email: ","sam@gmail.com"],
+			"city":				["Travel City: ","atlanta"],
+			"surveyCheckBoxes":	["Relationship to Owner: ","brother"],
+			"description":		["Leave a Comment: ","This is a test for Sam"],
+			"rating":			["Rate your Comfort Level: ","91"]
+
+
+
+			
+				
+		}
+
+}
+
